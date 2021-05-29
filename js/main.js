4247c1aac5cd66b2cc897fe3004156a4dc65278f
chugaev101 <chugaev101@gmail.com>
2021-05-29 20:31:48 +0300

    Adds a helper function to generate a random coordinate.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const getRandomNumber = (valueFrom, valueUpTo, numberDemicalPlaces) => {
+  if (valueFrom >= valueUpTo) {
+    return false;
+  }
+
+  const randomResult = Math.random() * (valueUpTo - valueFrom) + valueFrom;
+
+  return randomResult.toFixed(numberDemicalPlaces);
+};
